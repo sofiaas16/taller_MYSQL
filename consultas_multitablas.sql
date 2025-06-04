6339b7500771b5c8adb445adce033f3e3705fd24
sofiaas16 <sarahzambrano2019@gmail.com>
2025-06-04 11:43:10 -0500

    feat: :card_file_box: Ejercicio 2 JOINS

--- consultas_multitablas.sql
+++ consultas_multitablas.sql
@@ -5,3 +5,10 @@ SELECT usuarios.nombre, detalles_pedidos.pedido_id, detalles_pedidos.producto_id
 FROM usuarios
 JOIN pedidos ON pedidos.cliente_id = usuarios.usuario_id
 JOIN detalles_pedidos ON detalles_pedidos.pedido_id = pedidos.pedido_id;
+
+--2. Lista todos los productos pedidos junto con el precio unitario de cada pedido.
+SELECT p.pedido_id, GROUP_CONCAT(CONCAT(po.nombre,  ',',po.categoria)) AS productos, SUM(dp.cantidad * dp.precio_unitario) as Total
+FROM pedidos AS p   
+INNER JOIN detalles_pedidos AS dp ON p.pedido_id = dp.pedido_id
+INNER JOIN productos AS po ON dp.producto_id = po.producto_id
+GROUP BY p.pedido_id;
