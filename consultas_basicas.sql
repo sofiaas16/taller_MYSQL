1c030457376ef39e77b6fb83564bbbd87b119434
sofiaas16 <sarahzambrano2019@gmail.com>
2025-06-04 08:52:06 -0500

    feat: :sparkles: Ejercicio 4

--- consultas_basicas.sql
+++ consultas_basicas.sql
@@ -8,4 +8,8 @@ SELECT nombre, email FROM usuarios WHERE ciudad = 'Madrid';
 --3. Obtén una lista de productos con un precio mayor a $100.000, mostrando solo el nombre y el precio.
 SELECT nombre, precio FROM productos WHERE precio > 100000;
 
-
+--4. Encuentra todos los empleados que tienen un salario superior a $2.500.000, mostrando su nombre, puesto y salario.
+SELECT empleados.puesto, empleados.salario, usuarios.nombre 
+FROM usuarios
+JOIN empleados ON usuarios.usuario_id = empleados.usuario_id
+WHERE empleados.salario > 2500000;
