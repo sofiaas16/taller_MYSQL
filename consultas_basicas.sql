da8e29346083249592d311aa4f7626fa4dfba8b1
sofiaas16 <sarahzambrano2019@gmail.com>
2025-06-04 08:51:43 -0500

    feat: :sparkles: Ejercicio 3

--- consultas_basicas.sql
+++ consultas_basicas.sql
@@ -3,3 +3,9 @@ SELECT * FROM usuarios;
 
 -- 2. Muestra los nombre y correos electrónicos de todos los clientes que residen en la ciudad de Madrid.
 SELECT nombre, email FROM usuarios WHERE ciudad = 'Madrid';
+
+
+--3. Obtén una lista de productos con un precio mayor a $100.000, mostrando solo el nombre y el precio.
+SELECT nombre, precio FROM productos WHERE precio > 100000;
+
+
