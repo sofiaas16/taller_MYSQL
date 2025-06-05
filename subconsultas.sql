b0e01ede6c236a6a2f60d9b7cca7e643c8890078
sofiaas16 <sarahzambrano2019@gmail.com>
2025-06-05 17:19:27 -0500

    feat: :card_file_box: Ejercicio 1 y 2 SUBCONSULTAS

---
+++ subconsultas.sql
@@ -0,0 +1,21 @@
+-- Active: 1749153997759@@127.0.0.1@3307@Taller_MYSQL
+
+--1. Encuentra los nombres de los clientes que han realizado al menos un pedido de más de $500.000.
+SELECT u.nombre
+FROM usuarios u
+WHERE u.usuario_id IN (
+    SELECT p.cliente_id
+    FROM pedidos p
+    JOIN detalles_pedidos dp ON dp.pedido_id = p.pedido_id
+    WHERE dp.precio_unitario > 500000
+)
+
+--2. Muestra los productos que nunca han sido pedidos.
+SELECT p.nombre, p.producto_id
+FROM productos p
+WHERE p.producto_id NOT IN  (
+    SELECT dp.producto_id
+    FROM detalles_pedidos dp
+      
+    )
+
